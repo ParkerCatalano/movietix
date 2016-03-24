552ede5420ceb6a5c93982d4a2a607b34b6bbcd0
Ming Wen and Parker Catalano <parkercatalano@gmail.com>
2016-03-23 18:19:48 -0700

    Start rebuild app

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,21 @@
-//business logic
-function Movie(titles, time) {
-  this.titles = [];
-  this.time = [];
+function Ticket(title, time, price) {
+  this.title = title;
+  this.time = time;
+  this.price = price;
 }
 
-function Theater(movie, time) {
-  this.movie = [];
-  this.time = [];
+Ticket.prototype.age = function() {
+  var userAge = prompt("How old are you?");
 }
 
-var movieList = ["Revenant", "Deadpool", "Hail Caesar", "Brooklyn"];
-var movieTimes = ["9:00", "15:00", "22:00"];
-
-
-
-//jQuery
-$(document).ready(function(event) {
-
-
-    movieList.forEach(function(Movie) {
-      $("ul#listings").append("<li>" + movieList.toString() + "</li>");
-      $("#listings").last().click(function() {
-        $("#show-movieTimes").show(Movie.time);
-      });
-    });
-
-  });
+Ticket.prototype.discount = function() {
+  if (userAge <=17) {
+    return true;
+  }
+  else if (userAge > 55) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
